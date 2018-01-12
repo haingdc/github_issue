dda0b21f985ef4c7d321ec8d6b1c93db97df8f9c
haingdc <haingdc@gmail.com>
2018-01-12 08:42:44 +0700

    update mock data with time

--- src/mock_data.js
+++ src/mock_data.js
@@ -1,29 +1,96 @@
-export let issue = [
-  [
-    "First start take more than one minute, and modify one file take 10s to recompile. Is this ok?",
-  ],
-  ["set webpack-uglify-plugin ecma support based on browserlist"],
-  ["Misleading messages about script command changes when ejecting"],
-  ["Code splitting and preventing duplication"],
-  ["Proposal: explicit named imports for non-JS/CSS assets"],
-  ["Inform user they've specified a HOST"],
-  ['Add "debug-test" script by default'],
-  ["Rest properties are broken with Jest + node v6"],
-  ["'Npm start' fails on react-scripts-start initial attempt"],
-  ["ERROR calling Images from public folder"],
-  ["Documentation for NODE_PATH environment variable"],
-  ["Can't use NODE_PATH with two directories"],
-  ["Preparing the next release"],
-  ["Invalidate cache by service worker"],
-  ["Add WebWorker Support"],
-  ["Re-compile loop while starting dev server"],
-  ["CSS Loading Issue in development"],
-  ["Make it clearer in error overlay when error boundary has caught an error"],
-  ["Service worker fails to load new dynamic/code-split chunk"],
-  ['Debugging in Chrome and FireFox not evaluating "let" and "const" vars'],
-  ["Disable no-mixed-operators rule"],
-  ["Asset paths in CSS"],
-  ["[Question] Killing Process on Localhost with Cygwin"],
-  ["Problem with new builds"],
-  ["Symlink behaviour"],
+export let issues = [
+  {
+    title:
+      "First start take more than one minute, and modify one file take 10s to recompile. Is this ok?",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "set webpack-uglify-plugin ecma support based on browserlist",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Misleading messages about script command changes when ejecting",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Code splitting and preventing duplication",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Proposal: explicit named imports for non-JS/CSS assets",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Inform user they've specified a HOST",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: 'Add "debug-test" script by default',
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Rest properties are broken with Jest + node v6",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "'Npm start' fails on react-scripts-start initial attempt",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "ERROR calling Images from public folder",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Documentation for NODE_PATH environment variable",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Can't use NODE_PATH with two directories",
+    time: "12/10/2017, 8:17:17",
+  },
+  { title: "Preparing the next release", time: "12/10/2017, 8:17:17" },
+  { title: "Invalidate cache by service worker", time: "12/10/2017, 8:17:17" },
+  {
+    title: "Add WebWorker Support",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Re-compile loop while starting dev server",
+    time: "12/10/2017, 8:17:17",
+  },
+  { title: "CSS Loading Issue in development", time: "12/10/2017, 8:17:17" },
+  {
+    title:
+      "Make it clearer in error overlay when error boundary has caught an error",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Service worker fails to load new dynamic/code-split chunk",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title:
+      'Debugging in Chrome and FireFox not evaluating "let" and "const" vars',
+    time: "12/14/2017, 8:17:17",
+  },
+  {
+    title: "Disable no-mixed-operators rule",
+    time: "12/13/2017, 8:17:17",
+  },
+  {
+    title: "Asset paths in CSS",
+    time: "12/12/2017, 8:17:17",
+  },
+  {
+    title: "[Question] Killing Process on Localhost with Cygwin",
+    time: "12/12/2017, 8:17:17",
+  },
+  {
+    title: "Problem with new builds",
+    time: "12/10/2017, 8:17:17",
+  },
+  {
+    title: "Symlink behaviour",
+    time: "12/4/2017, 8:17:17",
+  },
 ];
