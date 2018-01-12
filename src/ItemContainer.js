f59cc09ae4f4c6311aafcc4082bd82f021690239
haingdc <haingdc@gmail.com>
2018-01-12 21:42:03 +0700

    finish 'issues' page - part one

--- src/ItemContainer.js
+++ src/ItemContainer.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { format, formatDistance, formatRelative, subDays } from "date-fns";
 
+if (process.env.NODE_ENV === "development") {
+  require("./ItemContainer.css");
+}
 class ItemContainer extends React.Component {
   render() {
-    let items = this.props.items.map((val, ind) => <li key={ind}>{val}</li>);
-    // console.log(items);
+    let d = format(new Date(), "[Today is a] dddd");
+    let items = this.props.items.map((issue, ind) => (
+      <li key={ind}>
+        <div className="float status">
+          <span className={issue.status}>{issue.status}</span>
+        </div>
+        <div className="float">
+          <a className="issue-item" href="#">
+            {issue.title}
+          </a>
+          <p>
+            {format(new Date(issue.time), "[Opened on ] MMM D, YYYY [by ]")}
+          </p>
+        </div>
+        <div class="clearfix" />
+      </li>
+    ));
+
     return (
       <div>
         <ul>{items}</ul>
