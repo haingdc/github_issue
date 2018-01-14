77c7b07eff0a0a4cbcc1e08f69664d19b2a11984
haingdc <haingdc@gmail.com>
2018-01-15 00:09:23 +0700

    Issue_Detail component display meta data

--- src/Issue_Detail.js
+++ src/Issue_Detail.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 
+if (process.env.NODE_ENV === "development") {
+  require("./IssueDetail.css");
+}
+
 class Issue_Detail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="about">
-        <h1>Specific Issue</h1>
+      <div className="issue-detail">
+        <h1>{this.props.issue_detail.title}</h1>
+        <div className="issue-detail__meta">
+          <span className="issue-detail__number">
+            #{this.props.issue_detail.issue_id}
+          </span>
+          <span className={this.props.issue_detail.status}>
+            {this.props.issue_detail.status}
+          </span>
+          <a href={this.props.issue_detail.author.profile_site}>
+            <img
+              className="issue__user__avatar"
+              src={this.props.issue_detail.author.avatar}
+            />
+            <div className="issue-detail__name">
+              {this.props.issue_detail.author.name}
+            </div>
+          </a>
+        </div>
       </div>
     );
   }
