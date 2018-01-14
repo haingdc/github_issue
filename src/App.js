3dc335a5d4308888c9f068d2ccf4f0824236eb21
haingdc <haingdc@gmail.com>
2018-01-14 13:34:53 +0700

    ready for making specific issue page

--- src/App.js
+++ src/App.js
@@ -15,17 +15,12 @@ const Navigation = () => (
     <ul>
       <li>
         <NavLink exact activeClassName="current" to="/">
-          Home
+          All Issues
         </NavLink>
       </li>
       <li>
-        <NavLink exact activeClassName="current" to="/about">
-          About
-        </NavLink>
-      </li>
-      <li>
-        <NavLink exact activeClassName="current" to="/contact">
-          Contact
+        <NavLink exact activeClassName="current" to="/specific-issue">
+          Specific Issue
         </NavLink>
       </li>
     </ul>
@@ -34,44 +29,31 @@ const Navigation = () => (
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/contact" component={Contact} />
+    <Route exact path="/" component={ISSUE} />
+    <Route exact path="/specific-issue" component={Specific} />
   </Switch>
 );
 
-const Home = () => (
-  <div className="home">
-    <h1>Welcome to my portfolio website</h1>
-    <p> Feel free to browse around and learn more about me.</p>
-  </div>
-);
-
-const About = () => (
+const Specific = () => (
   <div className="about">
-    <h1>About Me</h1>
-    <p>
-      Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-      corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
-      molestias?
-    </p>
-    <p>
-      Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
-      corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
-      molestias?
-    </p>
+    <h1>Specific Issue</h1>
   </div>
 );
 
-const Contact = () => (
-  <div className="contact">
-    <h1>Contact Me</h1>
-    <p>
-      You can reach me via email: <strong>hello@example.com</strong>
-    </p>
-  </div>
-);
 class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Navigation />
+          <Main />
+        </div>
+      </div>
+    );
+  }
+}
+
+class ISSUE extends Component {
   constructor(props) {
     super(props);
 
@@ -102,8 +84,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Main />
         <div className="container">
           <div className="text-center">
             <ItemContainer items={this.state.pageOfItems} />
