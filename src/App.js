29ea157490dd8bfaae7c62f3c7207b856654df07
haingdc <haingdc@gmail.com>
2018-01-14 11:21:23 +0700

    first try to react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
 import React, { Component } from "react";
+import _ from "underscore";
+import { NavLink, Switch, Route } from "react-router-dom";
 
 import ItemContainer from "./ItemContainer";
 import { issues } from "./mock_data";
 import Pagination from "./Pagination";
-import _ from "underscore";
 
 if (process.env.NODE_ENV === "development") {
   require("./App.css");
 }
 
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink exact activeClassName="current" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact activeClassName="current" to="/about">
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact activeClassName="current" to="/contact">
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/contact" component={Contact} />
+  </Switch>
+);
+
+const Home = () => (
+  <div className="home">
+    <h1>Welcome to my portfolio website</h1>
+    <p> Feel free to browse around and learn more about me.</p>
+  </div>
+);
+
+const About = () => (
+  <div className="about">
+    <h1>About Me</h1>
+    <p>
+      Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
+      corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
+      molestias?
+    </p>
+    <p>
+      Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident
+      corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum
+      molestias?
+    </p>
+  </div>
+);
+
+const Contact = () => (
+  <div className="contact">
+    <h1>Contact Me</h1>
+    <p>
+      You can reach me via email: <strong>hello@example.com</strong>
+    </p>
+  </div>
+);
 class App extends Component {
   constructor(props) {
     super(props);
@@ -40,12 +102,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navigation />
+        <Main />
         <div className="container">
           <div className="text-center">
             <ItemContainer items={this.state.pageOfItems} />
